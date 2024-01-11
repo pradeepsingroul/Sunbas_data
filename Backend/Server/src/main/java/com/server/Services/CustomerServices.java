db7949b729a22b580ee2cd093336ccb8c3cc8bfd
predeep kumar singroul <prasingroul4899@gmail.com>
2024-01-11 08:10:20 +0530

    Services

--- Backend/Server/src/main/java/com/server/Services/CustomerServices.java
+++ Backend/Server/src/main/java/com/server/Services/CustomerServices.java
@@ -1,6 +1,20 @@
 package com.server.Services;
 
+import java.util.List;
+
+import com.server.Exceptions.CustomerException;
+import com.server.Model.Customer;
 
 public interface CustomerServices {
-
+	
+	public Customer createCustomer(Customer c) throws CustomerException;
+	
+	public Customer getCustomerById(Integer id) throws CustomerException;
+	
+	public Customer deleteCustomerById(Integer id) throws CustomerException;
+	
+	public List<Customer> getAllCustomers() throws CustomerException;
+	
+	public Customer updateCustomerById(Integer id) throws CustomerException;
+	
 }
